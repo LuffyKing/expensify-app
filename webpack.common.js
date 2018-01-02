f3a0ea92e1a7b20991eb6a591f762aee22815fa6
LuffyKing <aderinwale17@gmail.com>
2018-01-02 19:19:21 +0100

    Add Expensify styles

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
-
+'adjustment'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV==='test'){
